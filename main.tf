0e3172917b957e10e2a5fddf69983a01ed9c8963
DeepakChandMarthala <deepakchand.marthala@gmail.com>
2024-12-06 12:48:55 -0500

    Force push all files to repository

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
+terraform {
+  required_version = ">= 1.0.0"  # Terraform version
+}
+
 provider "google" {
-  project = "YOUR_PROJECT_ID"
-  region  = "us-central1"
-  zone    = "us-central1-a"
+  project     = "adept-odyssey-443800-t2"  # Replace with your GCP Project ID
+  region      = "us-central1"             # Set your desired region
+  zone        = "us-central1-a"           # Set your desired zone
+  credentials = file("service-account.json")  # Path to your service account JSON file
 }
 
 resource "google_compute_instance" "ci_cd_instance" {
@@ -25,7 +30,7 @@ resource "google_compute_instance" "ci_cd_instance" {
     #!/bin/bash
     sudo apt-get update
     sudo apt-get install -y nginx
-    curl -o /var/www/html/index.html https://raw.githubusercontent.com/deepakchand.marthala/terraform-ci-cd/main/index.html
+    curl -o /var/www/html/index.html https://raw.githubusercontent.com/DeepakChandMarthala/GCP_Terraform/main/index.html
     sudo systemctl restart nginx
   EOF
 }
